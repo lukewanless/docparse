694b1b97c903667e4d4444c2a6aa9656c67700a4
lukewanless <u7120506@anu.edu.au>
2023-03-07 15:15:43 +1100

    Added basic davinci integration

--- app/parser/urls.py
+++ app/parser/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('error-handler/', views.error_handler, name='error_handler'),
 ]
 
--- app/parser/views.py
+++ app/parser/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+import openai
+import os
+import random
+
+openai.api_key = os.environ.get("OPENAI_KEY", "")
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World. You're at the parser index.")
+
+# this is the home view for handling home page logic
+def home(request):
+    try:
+        # checking if the request method is POST
+        if request.method == 'POST':
+            # getting prompt data from the form
+            prompt = request.POST.get('prompt')
+            # making a request to the API 
+            response = openai.Completion.create(model="text-davinci-003", prompt=prompt, max_tokens=1000)
+            # formatting the response input
+            text = random.choice(response["choices"])["text"]
+            # bundling everything in the context
+            context = {
+                'formatted_response': text,
+                'prompt': prompt
+            }
+            # this will render the results in the home.html template
+            return render(request, 'parser/home.html', context)
+        # this runs if the request method is GET
+        else:
+            # this will render when there is no request POST or after every POST request
+            return render(request, 'parser/home.html')
+    # the except statement will capture any error
+    except:
+        # this will redirect to the 404 page after any error is caught
+        return redirect('error_handler')
+ 
+# this is the view for handling errors
+def error_handler(request):
+    return render(request, 'parser/404.html') 
